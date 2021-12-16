95d1bdf890ae8083303e6b938ebfcedf60d05173
Simon <simon.duhem@gmail.com>
2021-12-16 22:36:58 +0100

    v1.1.0

--- content-script.js
+++ content-script.js
@@ -1,1 +1,8 @@
-window.location.href = `deezer://${window.location.pathname.substring(4)}`;
+if (/\bCrOS\b/.test(navigator.userAgent)) {
+  console.warn(
+    '[Open Deezer in desktop app] This script is not compatible with Chrome OS.',
+  );
+} else {
+  window.location.href = `deezer://${window.location.pathname.substring(4)}`;
+  window.close();
+}
